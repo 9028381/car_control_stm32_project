6efcc035fccbb38282ee7dd8b075d28a08cae17c
zl <2293721550@qq.com>
2025-02-03 20:22:04 +0800

    增加了BOM表，增加了PCB的readme，修改了一点点pid的小错误

--- User/Tool/pid.c
+++ User/Tool/pid.c
@@ -1,29 +1,34 @@
 #include "pid.h"
+
 #include "math.h"
 
-PID init_pid(float kp, float ki, float kd, float T, float integral_max)
-{
-    PID pid;
-    pid.kp = kp;
-    pid.ki = ki;
-    pid.kd = kd;
-    pid.T = T;
-    pid.integral_max = integral_max;
-    pid.error = 0;
-    pid.last_error = 0;
-    pid.integral = 0;
-    pid.derivative = 0;
-    
-    return pid;
+PID init_pid(float kp, float ki, float kd, float T, float integral_max) {
+  PID pid;
+  pid.kp = kp;
+  pid.ki = ki;
+  pid.kd = kd;
+  pid.T = T;
+  pid.integral_max = integral_max;
+  pid.error = 0;
+  pid.last_error = 0;
+  pid.integral = 0;
+  pid.derivative = 0;
+  pid.is_first = 1;
+
+  return pid;
 }
 
-float compute_pid(PID *pid, float error)
-{
-    pid->error = error;
-    pid->integral += pid->error * pid->T;
-    pid->integral = CONFINE(pid->integral, -pid->integral_max, pid->integral_max);
+float compute_pid(PID *pid, float error) {
+  pid->error = error;
+  pid->integral += pid->error * pid->T;
+  pid->integral = CONFINE(pid->integral, -pid->integral_max, pid->integral_max);
+  if (pid->is_first) {
+    pid->derivative = 0;
+    pid->is_first = 0;
+  } else {
     pid->derivative = (pid->error - pid->last_error) / pid->T;
-    pid->last_error = pid->error;
-    
-    return pid->kp * pid->error + pid->ki * pid->integral + pid->kd * pid->derivative;
+  }
+  pid->last_error = pid->error;
+
+  return pid->kp * pid->error + pid->ki * pid->integral + pid->kd * pid->derivative;
 }
--- User/Tool/pid.h
+++ User/Tool/pid.h
@@ -1,18 +1,18 @@
 #ifndef __PID_H
 #define __PID_H
 
-typedef struct PID
-{
-    float kp;
-    float ki;
-    float kd;
-    float T;          //采样周期
-    float error;
-    float last_error;
-    float integral;
-    float derivative;
-    float integral_max;
-}PID;
+typedef struct PID {
+  float kp;
+  float ki;
+  float kd;
+  float T;  // 采样周期
+  float error;
+  float last_error;
+  float integral;
+  float derivative;
+  float integral_max;
+  uint8_t is_first;
+} PID;
 
 PID init_pid(float kp, float ki, float kd, float T, float integral_max);
 float compute_pid(PID *pid, float error);
